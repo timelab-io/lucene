23e2f0275d1dc0d1fb07ece27c4646ca98de63d4
Cedric Lechevrel <cedrelek@gmail.com>
2017-12-17 10:06:28 +0100

    More permissive (no error on "fieldname:")

--- test/location_test.js
+++ test/location_test.js
@@ -5,14 +5,27 @@ const expect = require('chai').expect;
 const lucene = require('../');
 
 describe('queryParser', () => {
-  describe('whitespace handling', () => {
+  describe('timellab test', () => {
     // term parsing
-    it('handles empty string', () => {
+    it('handles location of field and term', () => {
       var results = lucene.parse('test:Foo');
       expect(results['left']['fieldLocation'].start.offset).to.equal(0);
       expect(results['left']['fieldLocation'].end.offset).to.equal(4);
       expect(results['left']['termLocation'].start.offset).to.equal(5);
       expect(results['left']['termLocation'].end.offset).to.equal(8);
+    });
+
+    it('fieldname without term', () => {
+      var results = lucene.parse('test:');
+      expect(results['left']['field']).to.equal('test');
+      expect(results['left']['fieldLocation'].start.offset).to.equal(0);
+      expect(results['left']['fieldLocation'].end.offset).to.equal(4);
+    });
+    it('just term', () => {
+      var results = lucene.parse('test');
+      expect(results['left']['term']).to.equal('test');
+      expect(results['left']['termLocation'].start.offset).to.equal(0);
+      expect(results['left']['termLocation'].end.offset).to.equal(4);
     });
   });
 });
